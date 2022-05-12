4a0cfe7396523500bfeef2c9ad21963fed96b085
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-05-12 19:23:41 +0400

    Seeds added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence true
-  validates :address, presence true
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
 
   restaurant_category = ["chinese", "italian", "japanese", "french", "belgian"]
 
-  validates :category, inclusion:{in:restaurant_category,message:"%{value} is not a valid category"}
+  validates :category, inclusion: {in: restaurant_category,message:"%{value} is not a valid category"}
 end
